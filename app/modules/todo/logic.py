8c482ce2b92ec150c1082a38c576080a59e3f16b
MT-Pavshii <mansurtokpaev@gmail.com>
2026-02-09 20:42:38 +0500

    add exit router

--- app/modules/todo/logic.py
+++ app/modules/todo/logic.py
@@ -1,11 +1,23 @@
-todo_list = []
+todo_list = {}
+todo_users = set()
 
-def add_item(item: str):
-    todo_list.append(item)
+def enter_todo(user_id: int):
+    todo_users.add(user_id)
 
-def get_item():
-    return todo_list
+def exit_todo(user_id: int):
+    todo_users.discard(user_id)
 
-def remove_item(index: int):
+def is_in_todo(user_id: int) -> bool:
+    return user_id in todo_users
+
+def add_item(user_id: int, item: str):
+    todo_list.setdefault(user_id, []).append(item)
+
+def get_item(user_id: int, item: str):
+    return todo_list.get(user_id, [])
+
+def remove_item(user_id: int, index: int):
+    items = todo_list.get(user_id)
     if 0 <= index < len(todo_list):
         todo_list.pop(index)
+
--- app/modules/todo/router.py
+++ app/modules/todo/router.py
@@ -1,5 +1,5 @@
 from aiogram import Router, types
-from modules.todo.logic import add_item, get_item, remove_item
+from modules.todo.logic import add_item, get_item, remove_item, enter_todo, exit_todo, is_in_todo
 from aiogram.filters import CommandStart, Command
 
 todo_router = Router()
@@ -7,6 +7,7 @@ todo_router = Router()
 
 @todo_router.message(lambda m: m.text == "ToDo")
 async def todo_entry (message: types.Message):
+    enter_todo(message.from_user.id)
     await message.answer(
         "TODO:\n"
         "• Напиши задачу, чтобы добавить\n"
@@ -16,12 +17,12 @@ async def todo_entry (message: types.Message):
 
 @todo_router.message(lambda m: m.text and not m.text.startswith("/"))
 async def add_task(message: types.Message):
-    add_item(message.text)
+    add_item(message.from_user.id, message.text)
     await message.answer(f"Задача добавлена: {message.text}")
 
 @todo_router.message(Command ("list"))
 async def list_tasks (message: types.Message):
-    items = get_item()
+    items = get_item(message.from_user.id)
     if not items:
         await message.answer ("Список пуст")
     else:
@@ -40,7 +41,12 @@ async def done_tasks (message: types.Message):
     
     if 0 <= index < len(items):
         removed = items[index]
-        remove_item(index)
+        remove_item(message.from_user.id, index)
         await message.answer(f"Задача выполнена и удалена: {removed}")
     else:
         await message.answer("Неверный номер задачи")
+
+@todo_router.message(Command ("exit"))
+async def exit_todo_cmd(message: types.Massage):
+    exit_todo(message.from_user.id)
+    await message.answer("Выходи из ToDo")
